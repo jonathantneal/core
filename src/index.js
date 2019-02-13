b31324bda587a04a6bfac95d5c9aeca55a6279ab
Matias Trujillo <mtrujillo@outlook.cl>
2019-02-12 22:57:37 -0300

    add export createContext

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import { update } from "./update";
 import { vnode } from "./vnode";
 export { options } from "./options";
 
+export { createContext } from "./createContext";
+
 export { useHook, useState, useEffect, useContext } from "./component";
 
 export { useRef, useMemo } from "./customHooks";
@@ -9,11 +11,17 @@ export { useRef, useMemo } from "./customHooks";
 export function h(tag, props, ...children) {
     return vnode(tag, props, children);
 }
-
-export function render(vnode, node, hostComponent) {
+/**
+ * this function allows you to manipulate elements from inside or from the container,
+ * to enable manipulation from the container you must define isHost as true.
+ * @param {object} vnode
+ * @param {HTMLElement|SVGElement} node
+ * @param {boolean} [isHost]
+ */
+export function render(vnode, node, isHost) {
     update(
         node,
-        hostComponent
+        isHost
             ? vnode
             : typeof vnode === "object" && vnode.tag === "host"
             ? vnode
